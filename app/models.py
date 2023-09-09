1f591dfa63f679eae7f8c9534ba8780f07a372ac
DashrathMore <dashrathmore990@gmail.com>
2023-09-09 12:38:19 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    sid=models.IntegerField(primary_key=True)
+    sname=models.CharField(max_length=100)
+    sage=models.IntegerField()
+    semail=models.EmailField()
--- app/views.py
+++ app/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from app.forms import *
 from django.http import HttpResponse
+from app.models import *
 
 # Create your views here.
-def student(request):
+def students(request):
     sfo=studentlogin()
     d={'sfo':sfo}
     if request.method=='POST':
         sfdo=studentlogin(request.POST)
         if sfdo.is_valid():
-            return HttpResponse(str(sfdo.cleaned_data))
+            sn=sfdo.cleaned_data['s_name']
+            si=sfdo.cleaned_data['s_id']
+            sa=sfdo.cleaned_data['s_age']
+            se=sfdo.cleaned_data['s_Email']
+            so=student.objects.get_or_create(sname=sn,sid=si, sage=sa, semail=se)[0]
+            so.save()
+            sdo=student.objects.all()
+            d={'sdo':sdo}
+            return render(request, 'display.html',d)
         else:
             return HttpResponse('Invalid data')
     return render(request,'student.html',d)
