d87f7b602d8c9a906af54a10fbacd66f17887977
DashrathMore <dashrathmore990@gmail.com>
2023-09-13 09:39:29 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,6 @@
+from typing import Any, Dict
 from django import forms
+from django.core import validators
 
 
 def name_valid(value):
@@ -9,10 +11,35 @@ def name_length(value):
     if len(value)<5:
         raise forms.ValidationError('name length is less than 5')
     
-
-
+sub=[('python','python'),('java','java'),('web_tech','web_tech'),('django','django'),('sql','sql')]
+gen=[('MALE','MALE'),('FEMALE','FEMALE'),('OTHERS','OTHERS')]
 class studentlogin(forms.Form):
-    s_name=forms.CharField(max_length=100,validators=[name_valid, name_length])
-    s_id=forms.IntegerField()
-    s_age=forms.IntegerField()
-    s_Email=forms.EmailField(validators=[name_valid])
+    sname=forms.CharField(max_length=100,validators=[name_valid, name_length])
+    sid=forms.IntegerField()
+    sage=forms.IntegerField()
+    semail=forms.EmailField(validators=[validators.RegexValidator('^[a-zA-Z0-9]\w*[.]?\w+@gmail[.]com')])
+    password=forms.CharField(max_length=12, widget=forms.PasswordInput)
+    gender=forms.ChoiceField(choices=gen,widget=forms.RadioSelect)
+    #gender=forms.ChoiceField(choices=gen)
+    #subjects=forms.MultipleChoiceField(choices=sub)
+    subjects=forms.MultipleChoiceField(choices=sub,widget=forms.CheckboxSelectMultiple)
+
+
+    #phone_number=forms.CharField(max_length=10, validators=[validators.RegexValidator('[6-9]\d{9}')])
+    #remail=forms.EmailField()
+    botchecker=forms.CharField(max_length=100,widget=forms.HiddenInput, required=False)
+
+    #def clean(self):
+      #  se=self.cleaned_data['semail']
+       # re=self.cleaned_data['remail']
+        #if se != re:
+         #   raise forms.ValidationError('email not matced')
+    
+    
+    
+    #clean_element method we cant use on normal input element it will give return as none
+    # its used to as botchecker a humen entering data from frontend or a autometed softwares inserting data by using source code
+    def  clean_botchecker(self):
+        bot=self.cleaned_data['botchecker']
+        if len(bot)>0:
+            raise forms.ValidationError('its not human')
--- app/views.py
+++ app/views.py
@@ -10,15 +10,16 @@ def students(request):
     if request.method=='POST':
         sfdo=studentlogin(request.POST)
         if sfdo.is_valid():
-            sn=sfdo.cleaned_data['s_name']
-            si=sfdo.cleaned_data['s_id']
-            sa=sfdo.cleaned_data['s_age']
-            se=sfdo.cleaned_data['s_Email']
+            """sn=sfdo.cleaned_data['sname']
+            si=sfdo.cleaned_data['sid']
+            sa=sfdo.cleaned_data['sage']
+            se=sfdo.cleaned_data['semail']
             so=student.objects.get_or_create(sname=sn,sid=si, sage=sa, semail=se)[0]
             so.save()
             sdo=student.objects.all()
-            d={'sdo':sdo}
-            return render(request, 'display.html',d)
+            d={'sdo':sdo}"""
+            #return render(request, 'display.html',d)
+            return HttpResponse(str(sfdo.cleaned_data))
         else:
             return HttpResponse('Invalid data')
     return render(request,'student.html',d)
